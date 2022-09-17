1a76e423cffd02010614c606d714b0ea470303f5
Ezra <fansoaezra@gmail.com>
2022-09-17 14:06:27 +0300

    add url

--- src/service/http.ts
+++ src/service/http.ts
@@ -2,8 +2,8 @@ import axios, { AxiosRequestConfig, AxiosError } from "axios";
 
 // axios.defaults.baseURL = import.meta.env?.VITE_API_BASE_URL;
 // console.log(import.meta.env?.VITE_API_BASE_URL);
-axios.defaults.baseURL = "https://virtserver.swaggerhub.com/hei.herizo/Hei-Admin/1.0.0/";
-// axios.defaults.baseURL = "http://localhost:8080/";
+// axios.defaults.baseURL = "https://virtserver.swaggerhub.com/hei.herizo/Hei-Admin/1.0.0/";
+axios.defaults.baseURL = "http://localhost:8080/";
 
 export class Http {
   static async get(url: string, params?: AxiosRequestConfig<any>) {
